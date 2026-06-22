8facd0893cece3b6c8b97656dea2647298717548
Viet Vo <vietvo@Viets-MacBook-Pro.local>
2026-06-23 03:52:28 +1000

    chore: remove non-system temporary python scripts from codebase

--- patch_arrow.py
+++
@@ -1,7 +0,0 @@
-with open("frontend/src/app/page.tsx", "r") as f:
-    page = f.read()
-
-page = page.replace("return `${workouts[0].date} ➡️ ${activePlan.race_date}`;", "return `${workouts[0].date} - ${activePlan.race_date}`;")
-
-with open("frontend/src/app/page.tsx", "w") as f:
-    f.write(page)
